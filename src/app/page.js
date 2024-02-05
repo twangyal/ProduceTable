7237d15c62a63acf07ff81ff41dafc033794ed6c
twangyal <tsering.w1029@gmail.com>
2024-02-04 22:01:06 -0800

    commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,103 @@
+'use client'
+import { useState } from 'react';
 export default function App(){
-
+  return (
+    <FilterableProductTable products={PRODUCTS}/>
+  );
 }
 function FilterableProductTable({ products }){
+  const [filterText, setFilterText]= useState("");
+  const [inStockOnly, setInStockOnly] = useState(false);
+  return(
+    <div>
+      <SearchBar filterText = {filterText} inStockOnly = {inStockOnly} 
+      onFilterTextChange={setFilterText} onInStockOnlyChange={setInStockOnly}/>
+      <ProductTable filterText = {filterText} inStockOnly = {inStockOnly} products = {products}/>
+    </div>
+  );
+}
+function SearchBar({filterText, inStockOnly, onFilterTextChange, onInStockOnlyChange}){
 
+  return(
+    <form>
+      <input type="text" value={filterText} placeholder="Search..." onChange={(e)=>onFilterTextChange(e.target.value)}/>
+      <label>
+        <input type="checkbox"
+        checked = {inStockOnly}
+        onChange={(e)=>onInStockOnlyChange(e.target.checked)}/>
+        
+        {' '}
+        Only show products in stock
+      </label>
+    </form>
+  );
 }
-function SearchBar(){
+function ProductTable({products, filterText, inStockOnly}){
+  const rows = [];
+  let lastCategory = null;
 
+  products.forEach((product) => {
+    if(product.name.toLowerCase().indexOf(filterText)===-1){
+      return;
+    }
+    if(inStockOnly&&!product.stocked){
+      return;
+    }
+    if(product.category!=lastCategory){
+      rows.push(
+        <ProductCategoryRow
+          category={product.category}
+          key={products.category}/>
+      );
+    }
+    rows.push(
+      <ProductRow product={product} key={product.name}/>
+    );
+      lastCategory = product.category;
+    }
+  );
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </table>
+  );
 }
-function ProductTable(){
-
+function ProductRow({product}){ 
+  const name = product.stocked ? product.name:
+  <span style={{color:"red"}}>
+    {product.name}
+  </span>;
+  return(
+  <tr> 
+    <td>
+      {name}
+    </td>
+    <td>
+      {product.price}
+    </td>
+  </tr>
+  );
 }
-function ProductRow(){
-
+function ProductCategoryRow({category}){
+  return(
+  <tr>
+    <th colSpan={2}>
+      {category}
+    </th>
+  </tr>
+  );
 }
-function ProductCategoryRow(){
-  
-}
+const PRODUCTS = [
+  {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
+  {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
+  {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
+  {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
+  {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
+  {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
+];
